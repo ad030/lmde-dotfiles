a20eb0e813074b6c10f1373198ccbb03aa0401ce
ad030 <68517956+ad030@users.noreply.github.com>
2024-07-24 22:35:45 -0400

    switched to wezterm from kitty

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- disable tab bar
+config.enable_tab_bar = false
+
+-- set terminal background opacity
+config.window_background_opacity = 0.7
+
+-- configure font
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+
+-- and finally, return the configuration to wezterm
+return config
