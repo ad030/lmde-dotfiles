62ded36f37c774e220632150c69d22fea0a96b02
ad030 <68517956+ad030@users.noreply.github.com>
2025-08-16 11:48:46 -0400

    use lobster update flag

--- .local/bin/update-lobster.sh
+++ .local/bin/update-lobster.sh
@@ -5,6 +5,9 @@ lobster -v
 sudo curl -sL github.com/justchokingaround/lobster/raw/main/lobster.sh -o /usr/local/bin/lobster &&
 sudo chmod +x /usr/local/bin/lobster
 
+# update script
+lobster --update
+
 echo "Update completed"
 echo "New lobster version:"
 lobster -v
