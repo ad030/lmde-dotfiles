817e4664e8232bd9e5655a0de7954b6622b41933
ad030 <68517956+ad030@users.noreply.github.com>
2025-08-16 11:46:07 -0400

    added one comment to update-lobster.sh

--- .local/bin/update-lobster.sh
+++ .local/bin/update-lobster.sh
@@ -1,6 +1,7 @@
 echo "Current lobster version:"
 lobster -v
 
+# install from source as described on github page
 sudo curl -sL github.com/justchokingaround/lobster/raw/main/lobster.sh -o /usr/local/bin/lobster &&
 sudo chmod +x /usr/local/bin/lobster
 
