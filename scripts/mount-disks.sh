9b15b000ae3512f65aa1d7aa9ff30ba402b9da3a
A.D. <work.a.dam.030@proton.me>
2025-12-01 23:26:53 -0500

    updated mount-disks.sh to mount and unmount, exit shortcut must first unmount disks before exiting

--- scripts/mount-disks.sh
+++ scripts/mount-disks.sh
@@ -2,6 +2,38 @@
 
 drive_uuids=("7A53-2FB6" "EC43-9BD1")
 
-for drive in ${drive_uuids[@]}; do
-	udisksctl mount --block-device /dev/disk/by-uuid/$drive --no-user-interaction
-done
+function mount_drives () {
+	for drive in ${drive_uuids[@]}; do
+		udisksctl mount --block-device /dev/disk/by-uuid/$drive --no-user-interaction
+	done
+}
+
+function unmount_drives() {
+	for drive in ${drive_uuids[@]}; do
+		udisksctl unmount --block-device /dev/disk/by-uuid/$drive --no-user-interaction
+	done
+}
+
+function bad_usage() {
+	echo "usage: $0 [mount|unmount]"
+	exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+	bad_usage
+fi
+
+case $1 in
+	"mount")
+		mount_drives
+		exit 0
+	;;
+	"unmount")
+		unmount_drives
+		exit 0
+	;;
+	*)	
+		bad_usage
+		exit 1
+	;;
+esac
